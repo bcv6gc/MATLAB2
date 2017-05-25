d9936d44abe934b88dc545cec79833f91ac11768
johnberns <itberns@gmail.com>
2017-05-25 12:19:49 -0500

    Added permittivity mixture algorithms, need to compare for our use case.

--- MaxwellGarnett.m
+++ MaxwellGarnett.m
@@ -1,2 +1,5 @@
-function active_perm = MaxwellGarnett(mixed_perm, media_perm, active_fraction)
+function active_perm = MaxwellGarnett(eff_perm, med_perm, active_volume)
+media_volume = 1-active_volume;
+active_perm = ((2+media_volume)*eff_perm - 2*active_volume*med_perm)...
+    .*med_perm./((1+2*media_volume)*med_perm - active_volume*eff_perm);
 end
